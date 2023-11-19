79c5d0d775f562589ef23f03823c4c4407b11fc6
Raghavendra M J <raghavendramj123@gmail.com>
2023-11-19 10:49:14 +0530

    OOPS Concepts!

---
+++ JavaScript/05_OOPS/03_GettersSetters.js
@@ -0,0 +1,34 @@
+//Constructor function
+function Product(price, brand, model) {
+  this.price = price;
+  this.brand = brand;
+  this.model = model;
+
+  var stocks = 10;
+
+  Object.defineProperty(this, "stks_avail", {
+    get: function () { 
+      return stocks;
+    },
+
+    set: function (value) {
+      //Check 1 -> Value -> EMPTY Check
+      if (value === "" || value === "undefined" || value === undefined) {
+        throw Error("Invalid stock information");
+      }
+
+      //Check 2 - Type
+      if (typeof value !== "number") {
+        throw Error("Invalid stock information - Its not a number");
+      } 
+      console.log("Adding new ", value, "stocks !");
+      stocks += value;
+    },
+  });
+}
+var apple = new Product(85000, "Apple", "IPhone14");
+console.log("apple ->", apple);
+
+console.log("1. apple.stks_avail ->", apple.stks_avail); //Invokes Getter
+apple.stks_avail = 4; //Invokes Setter 
+console.log("2. apple.stks_avail ->", apple.stks_avail); //Invokes Getter

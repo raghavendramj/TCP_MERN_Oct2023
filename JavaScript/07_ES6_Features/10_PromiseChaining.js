bba738328d80e40e6865af64db7b22c4116df902
Raghavendra M J <raghavendramj123@gmail.com>
2023-12-02 11:03:18 +0530

    Promise Chaining

--- JavaScript/07_ES6_Features/10_PromiseChaining.js
+++ JavaScript/07_ES6_Features/10_PromiseChaining.js
@@ -1,1 +1,78 @@
 //Usecase 1 :- Division -> Addition -> Multiplication -> Subtraction
+
+const add = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (arr.length < 2) {
+      reject("Unable to perform Addition");
+    } else {
+      let result = arr[0] + arr[1];
+      console.log("Sum is :- ", result);
+      resolve(arr);
+    }
+  });
+};
+
+const subtract = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (arr.length < 2) {
+      reject("Unable to perform Subtraction");
+    } else {
+      let result = arr[0] - arr[1];
+      console.log("Difference is :- ", result);
+      resolve(arr);
+    }
+  });
+};
+
+const multiply = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (arr.length < 2) {
+      reject("Unable to perform Multiplication");
+    } else {
+      let result = arr[0] * arr[1];
+      console.log("Product is :- ", result);
+      resolve(arr);
+    }
+  });
+};
+const division = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (arr.length < 2 && arr[1] != 0) {
+      reject("Unable to perform Division");
+    } else {
+      let result = arr[0] / arr[1];
+      console.log("Divsion is :- ", result);
+      resolve(arr);
+    }
+  });
+};
+
+let arr = [30, 16];
+
+ // Elaborative promise chaining
+let firstPromise = add(arr).then(
+  (successArr) => subtract(successArr),
+  (err) => console.log("Error", err)
+);
+
+let secondPromise = firstPromise.then(
+  (successArr) => multiply(successArr),
+  (err) => console.log("Error", err)
+);
+
+let thirdPromise = secondPromise.then(
+  (successArr) => division(successArr),
+  (err) => console.log("Error", err)
+);
+
+
+thirdPromise.then(
+  (successArr) => console.log("Success.... All promises have been completed!", successArr),
+  (err) => console.log("Error", err)
+);
+
+
+//One liner promise chaining!
+setTimeout(() => {
+    add(arr).then(subtract).then(division).then(multiply);
+}, 3000);

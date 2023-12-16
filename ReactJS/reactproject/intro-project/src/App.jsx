2455e841f3d280d2929b863d57b87e4001bddcc0
Raghavendra M J <raghavendramj123@gmail.com>
2023-12-16 10:06:50 +0530

    Bootstrap and React

---
+++ ReactJS/reactproject/intro-project/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div>
+    <div className="container mt-4">
+      <h1>This is from Bootstrap</h1>
+    </div>
+  </div>;
+}
+
+export default App;

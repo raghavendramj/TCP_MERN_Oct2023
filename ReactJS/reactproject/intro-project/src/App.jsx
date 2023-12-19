69f50ead147d729ae7644101f6b4a87725733e7e
Raghavendra M J <raghavendramj123@gmail.com>
2023-12-19 22:46:50 +0530

    React Project!

--- ReactJS/reactproject/intro-project/src/App.jsx
+++ ReactJS/reactproject/intro-project/src/App.jsx
@@ -1,12 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div>
+  return <React.Fragment>
     <div className="container mt-4">
-      <h1>This is from Bootstrap</h1>
+      <h1>Phone Directory!</h1>
+      <div>
+        <button className="btn btn-primary m-3">Add New Contact</button>
+      </div>
+      <table class="table table-striped table-bordered">
+        <thead class="bg-success text-white">
+          <tr>
+            <th>Name</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead> 
+      </table>
     </div>
-  </div>;
+
+
+  </React.Fragment>;
+
 }
 
 export default App;

bf83755af91575ea32bd71a6fc98ebfed91d8559
Raghavendra M J <raghavendramj123@gmail.com>
2023-12-23 10:20:55 +0530

    State change in class component

--- ReactJS/reactproject/intro-project/src/App.jsx
+++ ReactJS/reactproject/intro-project/src/App.jsx
@@ -4,11 +4,13 @@ import MyTestComponent from "./components/SimpleClassComponent";
 import Header from "./components/SecondClassComponent";
 import StylingInReact from "./components/Styling";
 import SimpleMapMethodDemo from "./components/UsingMapMethod";
+import ThirdClassComponent from "./components/ThirdClassComponent";
 
 function App() {
   return <React.Fragment>
     <div className="container mt-4">
-      <SimpleMapMethodDemo />
+      <ThirdClassComponent />
+      {/* <SimpleMapMethodDemo /> */}
       {/* <StylingInReact /> */}
       {/* <Header /> */}
       {/* <PhoneDirectory myName="Raghav" />  */}
---
+++ ReactJS/reactproject/intro-project/src/components/ThirdClassComponent.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class ThirdClassComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            customStyle: {
+                color: 'red',
+                fontSize: '20px',
+                padding: '10px',
+                backgroundColor: 'yellow'
+            }
+        }
+    }
+    changeColor = () => {
+        console.log("Invoked!");
+        const newStyle = {
+            color: 'green',
+            padding: '10px',
+            fontWeight: 'bold',
+            fontSize: '30px',
+            backgroundColor: 'orange'
+        }
+        this.setState({ customStyle: newStyle });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="container p-4 mt-3 text-center bg-primary text-white h1">
+                    State Change Example
+                </div>
+                <div className="container">
+                    <p style={this.state.customStyle}>This is how we change content</p>
+                    <button className="btn btn-primary" onClick={this.changeColor}>Change Color</button>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default ThirdClassComponent;

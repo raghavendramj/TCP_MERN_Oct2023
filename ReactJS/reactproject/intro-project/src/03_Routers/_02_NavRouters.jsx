993b7396227f4ab3806840f8b50509d84be2605f
Raghavendra M J <raghavendramj123@gmail.com>
2024-01-04 21:40:11 +0530

    Routers, with Navs etc..

--- ReactJS/reactproject/intro-project/src/03_Routers/_02_NavRouters.jsx
+++ ReactJS/reactproject/intro-project/src/03_Routers/_02_NavRouters.jsx
@@ -4,13 +4,27 @@ import { Link, Outlet } from "react-router-dom";
 function NavRouterLayout() {
     return (
         <>
-            <nav>
+            {/* <nav>
                 <ul>
                     <li><Link to="/home" ><button className="btn btn-primary m-3">Home</button></Link></li>
                     <li><Link to="/blogs"><button className="btn btn-secondary m-3">Blogs</button></Link></li>
                     <li><Link to="/contacts" ><button className="btn btn-danger m-3">Contacts</button></Link></li>
                 </ul>
-            </nav>
+            </nav> */}
+
+            <ul class="nav">
+                <li class="nav-item">
+                    <Link to="/home" ><button className="btn btn-primary m-2">Home</button></Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/blogs"><button className="btn btn-danger m-2">Blogs</button></Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/contacts" ><button className="btn btn-warning m-2">Contacts</button></Link>
+                </li>
+            </ul>
+
+
             <Outlet />
         </>
     );

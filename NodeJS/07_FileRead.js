4b2661c21fce42c42ce4a0188f8f4d27e2eba02b
Raghavendra M J <raghavendramj123@gmail.com>
2024-01-20 12:06:07 +0530

    File Read

---
+++ NodeJS/07_FileRead.js
@@ -0,0 +1,17 @@
+const fileSystem = require("fs");
+
+fileSystem.readFile("07_MyFile.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("Error reading the file! :- ", err);
+  } else {
+    console.log("Asyncronously reading data :- ", data);
+  }
+});
+
+
+let nonUTFFormattedData = fileSystem.readFileSync("07_MyFile.txt");
+console.log("1. Syncronously read - normal - non-utf-8 buffer data :- ", nonUTFFormattedData);
+console.log("2. Syncronously read - normal - non-utf-8 data string :- ", nonUTFFormattedData.toString());
+
+let uTFFormattedData = fileSystem.readFileSync("07_MyFile.txt", "utf-8");
+console.log("3. Syncronously read - utf-8 - formatted data", uTFFormattedData); 

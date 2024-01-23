9d85e7895393410e6a933053d936a7f818e08bf9
Raghavendra M J <raghavendramj123@gmail.com>
2024-01-23 22:08:29 +0530

    POST

--- NodeJS/expressjs/02_ReqFields.js
+++ NodeJS/expressjs/02_ReqFields.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const application = express();
 const authors = require("./00_authors.json");
+const bodyParser = require("body-parser");
 
 application.get("/api/authors", (req, res) => {
   res.send(authors);
 });
 
-application.get("/api/authors/:id", (req, res) => {
+application.get("/api/author/:id", (req, res) => {
   const authorId = Number(req.param("id"));
   const filteredAuthors = authors.filter((author) => author.id === authorId);
 
@@ -15,6 +16,26 @@ application.get("/api/authors/:id", (req, res) => {
   } else {
     res.send("No Authors found, with this id :-  " + authorId);
   }
+});
+
+application.use(bodyParser.json());
+application.post("/api/author/", (req, res) => {
+  console.log("query -> ", req.body);
+  const { key, value } = req.body;
+
+  if (!key || !value) {
+    res.send("Both key and value are required to continue!");
+  }
+
+  const filteredAuthors = authors.filter((author) => author[key] == value);
+
+  if (filteredAuthors.length > 0) {
+    res.send(filteredAuthors[0]);
+  } else {
+    res.send("No Authors found, with this key :-  " + value);
+  }
+
+  res.send("query");
 });
 
 application.listen(8082);

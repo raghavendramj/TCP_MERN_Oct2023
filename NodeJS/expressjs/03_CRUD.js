4d7dd5b50156e55f8ffba98656796f02762c17bd
Raghavendra M J <raghavendramj123@gmail.com>
2024-01-23 22:44:30 +0530

    CRUD

---
+++ NodeJS/expressjs/03_CRUD.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+let movies = require("./00_movies.json");
+
+//CRUD -> Create - Read - Update - Delete
+
+//Create
+app.use(express.json());
+app.post("/api/movies", (req, res) => {
+  const movie = req.body;
+
+  console.log("Movie :- ", movie);
+
+  if (!movie.director || !movie.title || !movie.genre) {
+    res.send("director, title and genre are required!");
+  }
+  const movieNewObj = { id: movies.length + 1, ...movie };
+  movies.push(movieNewObj);
+  res.send(movieNewObj);
+});
+
+//Read
+app.get("/api/movies", (req, res) => {
+  res.send(movies);
+});
+
+app.listen(8082);
+
+//Update
+app.use(express.json());
+app.put("/api/movies", (req, res) => {
+  const movie = req.body;
+  if (
+    !movie.id ||
+    !movie.director ||
+    !movie.title ||
+    !movie.genre ||
+    !movie.releaseYear ||
+    !movie.rating
+  ) {
+    res.send("director, title and genre are required!");
+  }
+
+  movies = movies.map((eachMovie) => {
+    if (eachMovie.id == movie.id) {
+      return movie;
+    } else {
+      return eachMovie;
+    }
+  });
+
+  res.send(movies.find((eachMovie) => eachMovie.id == movie.id));
+});
+
+//Delete
+app.delete("/api/movies/:id", (req, res) => {
+  const movieId = Number(req.param("id"));
+  movies = movies.filter((eachMovie) => eachMovie.id != movieId);
+  res.send(movies);
+});
